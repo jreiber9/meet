8ab4118fb3c9adec82d2b3fe7e06ce8bd6306e2d
jreiber9 <128739362+jreiber9@users.noreply.github.com>
2023-09-15 17:08:28 -0400

    Removed Atatus test code

--- src/index.js
+++ src/index.js
@@ -23,4 +23,6 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-atatus.notify(new Error('Test Atatus Setup'));
+
+// Atatus test
+// atatus.notify(new Error('Test Atatus Setup'));
